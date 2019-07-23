bd9d3bceaf168e79770d51af8e86d15457095729
Barry <huruyang@tengyue360.com>
2019-07-23 11:26:46 +0800

    xuanran

--- serve/app.js
+++ serve/app.js
@@ -6,6 +6,13 @@ const get = require('./router/get.js')
 const post = require('./router/post.js')
 const bodyParser = require('koa-bodyparser')
 const views = require('koa-views')
+const onerror = require('koa-onerror');
+//开发环境渲染
+const koaWebpack = require('koa-webpack');
+const webpackDevConf = require('./../build/webpack.dev.conf')
+const middleware = async()=>{await koaWebpack(webpackDevConf)};
+app.use(middleware);
+
 // const render = require('koa-art-template')
 const static = require('koa-static')
 const fs = require('fs')
@@ -22,7 +29,10 @@ require('./sort')
 //ejs渲染页面
 app.use(views(__dirname + './../dist', {
     map : {html:'ejs'}
-  }));
+  }))
+
+//错误信息处理
+onerror(app);
 
 app.use(async(ctx,next)=>{
     ctx.state.username = '张三'
@@ -42,9 +52,9 @@ app.use(async (ctx, next) => {
 });
 
 //配置静态web服务的中间件
-app.use(static(
-    path.join( __dirname,'./../dist')
-))
+// app.use(static(
+//     path.join( __dirname,'./../dist')
+// ))
 
 //渲染页面
 router.get('/',async (ctx)=>{
